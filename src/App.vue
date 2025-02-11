d82192f3b1efdd64415c8b82fd539a53368bacf6
llh <lianglh_only@163.com>
2025-02-11 16:58:21 +0800

    图标引入

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-
+  
 </script>
 
 <template>
-    <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
+  <!-- 一级路由出口 -->
+  <RouterView/>
+
 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
 
 </style>
 
