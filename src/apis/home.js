ef226f60ec856248098bc4fbf5cf1671c8d9d3dc
llh <lianglh_only@163.com>
2025-02-12 17:28:16 +0800

    热门推荐

---
+++ src/apis/home.js
@@ -0,0 +1,19 @@
+import httpInstance from "@/utils/http"
+
+export function getBannerAPI() {
+    return httpInstance({
+        url: '/home/banner'
+    })
+}
+
+export function findNewAPI() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+export function findHotAPI() {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>homeProduct</h1>
+</template>
