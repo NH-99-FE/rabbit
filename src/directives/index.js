a8263de543f2452a187c6560c1938e134d451453
llh <lianglh_only@163.com>
2025-02-13 10:58:34 +0800

    图片懒加载优化

--- src/directives/index.js
+++ src/directives/index.js
@@ -7,12 +7,13 @@ export const lazyPlugin = {
             mounted(el , binding) {
                 // el: 指令所绑定的元素
                 // binding: binding.value 指令等于号后面绑定的表达式的值：Url
-                useIntersectionObserver(
+                const { stop } = useIntersectionObserver(
                     el,
                     ([{isIntersecting}]) => {
                     if(isIntersecting){
                         // 进入视口区域
                         el.src=binding.value
+                        stop() //停止监听
                     }
                     },
                 )
