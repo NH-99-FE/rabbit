41c0c888a8da71673a39122a68dd05c3d5790769
llh <lianglh_only@163.com>
2025-02-20 11:29:01 +0800

    401token失效拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { ElMessage } from "element-plus"
 import { useUserStore } from "@/stores/user"
+import router from "@/router"
 
 
 // 创建axios实例
@@ -23,11 +24,17 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  // 401token失效处理
+  if(e.response.status === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
