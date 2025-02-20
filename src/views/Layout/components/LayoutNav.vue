dcb3d8d3711209c4519a3131e5cccfb7cf039b48
llh <lianglh_only@163.com>
2025-02-20 11:06:43 +0800

    登录-退出登录业务实现

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
 const userStore = useUserStore()
 const { userInfo } = storeToRefs(userStore)
+
+const router = useRouter()
+
+// 退出用户业务
+const confirm = () => {
+  userStore.clearUserInfo()
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -13,7 +22,7 @@ const { userInfo } = storeToRefs(userStore)
         <template v-if="userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userInfo.account  }}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
